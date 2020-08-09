8ef9c0beeb48d3424028c3236f6d56e7133854a5
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-08 19:03:25 -0700

    Use composite run steps

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM buildpack-deps:18.04-scm
-COPY entrypoint.sh /entrypoint.sh
-COPY merge.sh /merge.sh
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-uid="$(stat --format=%u .)"
-useradd --uid="$uid" --create-home github-action
-exec su github-action /merge.sh "$@"
